4adeac231e6ab7bab653ea379fc6be80e0d50220
gangotrigupta-61 <gangotrigupta61@gmail.com>
2026-08-18 11:22:02 +0530

    Done with DOM

--- DOM/selecting_manipulating.js
+++ DOM/selecting_manipulating.js
@@ -88,7 +88,41 @@ console.dir(document.querySelectorAll(".imgclass"));
 
 // manipulating attributes
 
+// let img=document.querySelector('img');
+// undefined
+// img
+// <img src=​"image2.jpeg" id=​"images">​
+// img.getAttribute('id');
+// 'mainImage'
+// img.setAttribute('id',"images");                    styling is removed
+// undefined
+// img.getAttribute('src');      
+// 'image1.jpeg'
+// img.setAttribute('src',"image2.jpeg");
+// undefined
 
+
+let para1=document.createElement('p');
+para1.innerText="Hey i am red";
+document.querySelector("body").append(para1);
+para1.classList.add("red");
+
+let h3=document.createElement("h1");
+h3.innerText="I am a blue h3!";
+document.querySelector("body").append(h3);
+h3.classList.add("blue");
+
+
+let div=document.createElement("div");
+let h1=document.createElement("h1");
+let para2=document.createElement("p");
+h1.innerText="I am a div";
+para2.innerText=" Me too!!";
+
+div.append(h1);
+div.append(para2);
+div.classList.add('box');
+document.querySelector("body").append(div);
 
 
 
